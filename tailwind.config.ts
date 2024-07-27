a2ce329c701d84f686d0852c6da9aa9c1f91c3c6
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-26 23:40:31 -0300

    feat: ajusta projeto para começar o desenvolvimento do zero

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primaria: "#0096AA",
+        secundaria: "#004F5C",
+      }
     },
   },
   plugins: [],
